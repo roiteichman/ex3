2c09d34978e2ff82bb1d9e20ae9df55179d3b922
teich <roi.teichman@campus.technion.ac.il>
2022-05-23 18:09:56 +0300

    Queue.h

---
+++ Queue.h
@@ -0,0 +1,21 @@
+//
+// Created by teich on 23/05/2022.
+//
+
+#ifndef EX3_QUEUE_H
+#define EX3_QUEUE_H
+
+template<class T>
+class Queue{
+public:
+
+
+
+private:
+
+
+
+};
+
+
+#endif //EX3_QUEUE_H
